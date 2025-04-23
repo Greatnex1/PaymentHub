5f1ebe7c1243f7274d32fe39b8217734e8633155
Greatnex1 <noahgreatakoni@gmail.com>
2025-04-23 09:16:04 +0100

    add paystack constant

---
+++ src/main/java/com/payu/payment_hub/model/constants/PaystackConstants.java
@@ -0,0 +1,19 @@
+package com.payu.payment_hub.model.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaystackConstants {
+    public static final Integer STATUS_CODE_OK = 200;
+    public static final Integer STATUS_CODE_CREATED = 201;
+    public static final String PAYSTACK_PLAN = "https://api.paystack.co/plan";
+    public static final String PAYSTACK_RECEIVE_PAYMENT = "https://api.paystack.co/transaction/initialize";
+    public static final String PAYSTACK_VERIFY = "https://api.paystack.co/transaction/verify/";
+    public static final String PAYSTACK_SEND_PAYMENT = "https://api.paystack.co/transfer";
+    public static final String PAYSTACK_VALIDATE_ACCOUNT_NUMBER = "https://api.paystack.co/bank/resolve?account_number=";
+    public static final String PAYSTACK_LIST_OF_BANKS = "https://api.paystack.co/";
+    public static final String PAYSTACK_LIST_OF_COUNTRY = "https://api.paystack.co/country";
+    public static final String PAYSTACK_LIST_OF_STATES = "https://api.paystack.co/address_verification/states?country=";
+    public static final String PAYSTACK_CREATE_RECEIPIENT = "https://api.paystack.co/transferrecipient";
+    public static final String PAYSTACK_FINALIZE_TRANSACTION = "https://api.paystack.co/transfer/finalize_transfer";
+}
